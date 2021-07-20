0f9c53ade77ba517b8ad60297e646ce8a8e95440
brightonyoung <brightonhy@gmail.com>
2021-07-20 00:50:28 -0500

    gallery tab

---
+++ index.js
@@ -0,0 +1,1 @@
+<iframe width="713" height="401" src="https://www.youtube.com/embed/-ynEco3IwVY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
